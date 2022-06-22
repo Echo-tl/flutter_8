21224dd82dcb42d87222124b881ae71eef816802
Echo-tl <428832129@qq.com>
2022-06-22 09:03:33 +0000

    add

---
+++ lib/main.dart
@@ -0,0 +1,232 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //home: const StackEventTest(),
+      //home: const _GestureRecognizer(),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: _BothDirectionTest(),
+      home: NotificationRoute(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+//手势识别
+class _GestureRecognizer extends StatefulWidget {
+  const _GestureRecognizer({Key? key}) : super(key: key);
+
+  @override
+  _GestureRecognizerState createState() => _GestureRecognizerState();
+}
+
+class _GestureRecognizerState extends State<_GestureRecognizer> {
+  TapGestureRecognizer _tapGestureRecognizer = TapGestureRecognizer();
+  bool _toggle = false; //变色开关
+
+  @override
+  void dispose() {
+    //用到GestureRecognizer的话一定要调用其dispose方法释放资源
+    _tapGestureRecognizer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text.rich(
+        TextSpan(
+          children: [
+            TextSpan(text: "你好世界"),
+            TextSpan(
+              text: "点我变色",
+              style: TextStyle(
+                fontSize: 30.0,
+                color: _toggle ? Colors.blue : Colors.red,
+              ),
+              recognizer: _tapGestureRecognizer
+                ..onTap = () {
+                  setState(() {
+                    _toggle = !_toggle;
+                  });
+                },
+            ),
+            TextSpan(text: "你好世界"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//flutter事件机制(实现App水印)
+class StackEventTest extends StatelessWidget {
+  const StackEventTest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        wChild(1),
+        wChild(2),
+      ],
+    );
+  }
+
+  Widget wChild(int index) {
+    return Listener(
+      onPointerDown: (e) => print(index),
+      child: Container(
+        width: 100,
+        height: 100,
+        color: Colors.grey,
+      ),
+    );
+  }
+}
+
+//手势竞争
+class _BothDirectionTest extends StatefulWidget {
+  @override
+  _BothDirectionTestState createState() => _BothDirectionTestState();
+}
+
+class _BothDirectionTestState extends State<_BothDirectionTest> {
+  double _top = 0.0;
+  double _left = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          top: _top,
+          left: _left,
+          child: GestureDetector(
+            child: CircleAvatar(child: Text("A")),
+            //垂直方向拖动事件
+            onVerticalDragUpdate: (DragUpdateDetails details) {
+              setState(() {
+                _top += details.delta.dy;
+              });
+            },
+            onHorizontalDragUpdate: (DragUpdateDetails details) {
+              setState(() {
+                _left += details.delta.dx;
+              });
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
+
+//分发通知
+class NotificationRoute extends StatefulWidget {
+  @override
+  NotificationRouteState createState() {
+    return NotificationRouteState();
+  }
+}
+
+class NotificationRouteState extends State<NotificationRoute> {
+  String _msg = "";
+  @override
+  Widget build(BuildContext context) {
+    //监听通知
+    return NotificationListener<MyNotification>(
+      onNotification: (notification) {
+        setState(() {
+          _msg += notification.msg + "  ";
+        });
+        return true;
+      },
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+//           ElevatedButton(
+//           onPressed: () => MyNotification("Hi").dispatch(context),
+//           child: Text("Send Notification"),
+//          ),
+            Builder(
+              builder: (context) {
+                return ElevatedButton(
+                  //按钮点击时分发通知
+                  onPressed: () => MyNotification("Hi").dispatch(context),
+                  child: Text("Send Notification"),
+                );
+              },
+            ),
+            Text(_msg)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyNotification extends Notification {
+  MyNotification(this.msg);
+  final String msg;
+}
